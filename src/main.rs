4c783fece11c77899d66af65fe4b79dc01f66467
Andreas Åman <andreas.aman@atg.se>
2024-08-08 14:43:38 +0200

    Add parsing of arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Username
+    #[arg(short, long)]
+    username: String,
+
+    /// Path to highscore file
+    #[arg(short, long, default_value = "highscore.txt")]
+    path: String,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
